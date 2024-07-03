bdd87942e8073a7f6e8e1efba44044a0352e6424
Anantpandey07 <anantpandey175@gmail.com>
2024-07-03 14:16:49 +0530

    update

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -69,8 +69,7 @@ function Form() {
                     <option value="investments">Investments</option>
                     <option value="stocks">Stocks</option>
                     <option value="bitcoin">Bitcoin</option>
-                    <option value="bank">Bank Transfer</option>  
-                    <option value="youtube">Youtube</option>  
+                    <option value="bank">Bank Transfer</option>   
                     <option value="other">Other</option>  
                 </select>
             </div>
